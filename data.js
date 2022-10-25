5c7455b3f3bf9196f0dd0155cfae72dffb2df020
rachelBonanno <rachel.bonanno@tufts.edu>
2022-10-25 11:47:06 -0400

    stuff

---
+++ data.js
@@ -0,0 +1,18 @@
+fetch('package.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Artist: ' + data[i].artist + '\n';
+        mainContainer.appendChild(div);
+    }
+}
